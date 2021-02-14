2d03b28f160f39e5907598009378ee1bb5152a13
jgrewal <jagnoorg@jagnoorgs-MacBook-Pro.local>
2021-02-14 10:39:28 +0530

    Updates

---
+++ precache-manifest.005942806cee7f3cdd3a02950a508de0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a725d1cfdc1749b78d5e0af714418a54",
+    "url": "/%7Beditor%7D/index.html"
+  },
+  {
+    "revision": "cbc64d6154190731d713",
+    "url": "/%7Beditor%7D/static/css/2.a309cce6.chunk.css"
+  },
+  {
+    "revision": "f2e7f77276f2b8e9cba2",
+    "url": "/%7Beditor%7D/static/css/main.4c26728a.chunk.css"
+  },
+  {
+    "revision": "cbc64d6154190731d713",
+    "url": "/%7Beditor%7D/static/js/2.5293676d.chunk.js"
+  },
+  {
+    "revision": "538bc390c210ed9b00728e9e871340d6",
+    "url": "/%7Beditor%7D/static/js/2.5293676d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c044f658610899f440e",
+    "url": "/%7Beditor%7D/static/js/3.ed6ef750.chunk.js"
+  },
+  {
+    "revision": "f2e7f77276f2b8e9cba2",
+    "url": "/%7Beditor%7D/static/js/main.571217f6.chunk.js"
+  },
+  {
+    "revision": "feac66465cc2c08f919a",
+    "url": "/%7Beditor%7D/static/js/runtime-main.92e84d50.js"
+  }
+]);
